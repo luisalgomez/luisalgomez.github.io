37366da1abfd507ad86f5a25f99d3e82b89733ae
luisalgomez <150172851+luisalgomez@users.noreply.github.com>
2024-06-21 20:47:32 -0400

    Biblioteca Virtual ECAB
    Primera Impresión del Producto

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".articulo").forEach(libro =>{
+  
+            libro.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?libro.classList.remove("filtro")
+              :libro.classList.add("filtro")
+        })
+  
+    }
+  
+  
+  })
